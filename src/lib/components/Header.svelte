0d2a8d36af813062e62426cef2df04b2e07bdcec
Jerome Paulos <jeromepaulos@icloud.com>
2023-06-14 20:00:35 -0700

    Make header sticky

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<div class="bg-blue-500 p-1 text-white flex gap-4 items-center justify-between leading-none">
+<div class="bg-blue-500 p-1 text-white flex gap-4 items-center justify-between leading-none sticky top-0 z-10">
     <a href="/" class="flex gap-2 items-center">
         <img class="w-5 h-5 border border-white" src="/assets/icon.svg" alt="">
         <p class="font-bold">Horizontal News</p>
