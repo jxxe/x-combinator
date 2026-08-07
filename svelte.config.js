629e29e489dab0705acbd1cec0dba5ef9cc0dfb8
Jerome Paulos <jeromepaulos@icloud.com>
2026-08-06 20:08:56 -0700

    Use server adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ fallback: '200.html' })
+		adapter: adapter()
 	}
 };
 
